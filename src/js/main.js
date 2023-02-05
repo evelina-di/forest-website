e9bdbdfbe3433d4eecc88207b6cc62b67d65fc81
evelina-di <dikta.e@gmail.com>
2023-02-05 18:44:10 +0100

    footer i zmiany klasy burger icon

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,57 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const nav = document.querySelector(".nav");
 const navLinks = document.querySelectorAll(".nav_link");
+const footerYear = document.querySelector(".footer__year");
+const allSections = document.querySelectorAll(".section");
 
 const handleBurgerMenu = () => {
 	nav.classList.toggle("active");
+
+	if (nav.classList.contains("active")) {
+		burgerIcon.classList.add("burger-black");
+	} else if (!nav.classList.contains("active")) {
+		burgerIcon.classList.remove("burger-black");
+	}
 };
 
 const handleMobileMenu = () => {
 	navLinks.forEach((link) => {
 		link.addEventListener("click", () => {
 			nav.classList.remove("active");
-			console.log("clicked");
+			burgerIcon.classList.remove("burger-black");
 		});
 	});
 };
+
+const showMe = () => {
+	console.log(window.scrollY);
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			burgerIcon.classList.add("burger-black");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			burgerIcon.classList.remove("burger-black");
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
 handleMobileMenu();
+
+window.addEventListener("scroll", handleObserver);
 burgerIcon.addEventListener("click", handleBurgerMenu);
+handleCurrentYear();
