9b8d27c2fbccc25ebac2884c0a925dfabe536a31
evelina-di <dikta.e@gmail.com>
2023-02-07 20:58:11 +0100

    zmiana hamburgera, dodanie zakladki kontakt

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,32 @@
-const burgerIcon = document.querySelector(".burger-icon");
+const burgerIcon = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
-const navLinks = document.querySelectorAll(".nav_link");
+const navLinks = document.querySelectorAll(".nav__link");
 const footerYear = document.querySelector(".footer__year");
 const allSections = document.querySelectorAll(".section");
+const header = document.querySelector(".header");
+const emailInput = document.querySelector("#mail");
+const messageArea = document.querySelector("#question");
+const emailError = document.querySelector(".contact__form-error");
+const thankyouMessage = document.querySelector(".contact__form-thankyou");
+const submitBtn = document.querySelector(".submit");
 
 const handleBurgerMenu = () => {
 	nav.classList.toggle("active");
 
 	if (nav.classList.contains("active")) {
-		burgerIcon.classList.add("burger-black");
+		burgerIcon.classList.add("is-active");
 	} else if (!nav.classList.contains("active")) {
-		burgerIcon.classList.remove("burger-black");
+		burgerIcon.classList.remove("is-active");
+	}
+};
+
+console.log(window.innerWidth);
+
+const handleMobHeader = () => {
+	if (window.location.href.indexOf("contact") > -1 && window.innerWidth < 768) {
+		header.style.display = "none";
+	} else {
+		header.style.display = "flex";
 	}
 };
 
@@ -18,31 +34,31 @@ const handleMobileMenu = () => {
 	navLinks.forEach((link) => {
 		link.addEventListener("click", () => {
 			nav.classList.remove("active");
-			burgerIcon.classList.remove("burger-black");
+			burgerIcon.classList.remove("is-active");
 		});
 	});
 };
 
-const showMe = () => {
-	console.log(window.scrollY);
+const checkEmail = () => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	if (!re.test(emailInput.value)) {
+		emailError.style.visibility = "visible";
+	} else {
+		emailError.style.visibility = "hidden";
+		clearContent();
+		showThankYouMessage();
+	}
 };
 
-const handleObserver = () => {
-	const currentSection = window.scrollY;
+const clearContent = () => {
+	emailInput.value = "";
+	messageArea.value = "";
+};
 
-	allSections.forEach((section) => {
-		if (
-			section.classList.contains("white-section") &&
-			section.offsetTop <= currentSection + 60
-		) {
-			burgerIcon.classList.add("burger-black");
-		} else if (
-			!section.classList.contains("white-section") &&
-			section.offsetTop <= currentSection + 60
-		) {
-			burgerIcon.classList.remove("burger-black");
-		}
-	});
+const showThankYouMessage = () => {
+	thankyouMessage.style.visibility = "visible";
 };
 
 const handleCurrentYear = () => {
@@ -51,7 +67,7 @@ const handleCurrentYear = () => {
 };
 
 handleMobileMenu();
-
-window.addEventListener("scroll", handleObserver);
 burgerIcon.addEventListener("click", handleBurgerMenu);
 handleCurrentYear();
+handleMobHeader();
+submitBtn.addEventListener("click", checkEmail);
